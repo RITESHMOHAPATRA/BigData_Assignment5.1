258d04927df47bac1980f047b73afd81720f1c47
RITESHMOHAPTRA <mohapatraritesh97@gmail.com>
2018-06-08 22:59:25 +0530

    final commit

--- Assignment_5.1/src/Gcd.scala
+++ Assignment_5.1/src/Gcd.scala
@@ -3,7 +3,9 @@
 object Gcd {
   def main(args: Array[String]){
     val res = gcd(args(0).toInt,args(1).toInt)
+    //command line arguments passed to gcd function
     println("GCD of "+args(0)+" and "+args(1)+" is "+res)
   }
+  //Gcd function to calculate gcd of 2 nos.
   def gcd(a:Int,b:Int): Int = if (b==0) a else gcd(b,a%b)
 }
